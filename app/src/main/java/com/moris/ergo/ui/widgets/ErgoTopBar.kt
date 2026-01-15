d88e47476f46055169554167a286aa8d50be4a92
moris071651 <moris071651@proton.me>
2026-01-15 08:41:34 +0200

    ADD: setup the top bar

---
+++ app/src/main/java/com/moris/ergo/ui/widgets/ErgoTopBar.kt
@@ -0,0 +1,53 @@
+package com.moris.ergo.ui.widgets
+
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.font.FontWeight
+
+data class ErgoTopBarAction(
+    val icon: ImageVector,
+    val onClick: () -> Unit,
+    val description: String? = null
+)
+
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+fun ErgoTopBar(
+    title: String,
+    rightAction: ErgoTopBarAction? = null,
+    leftAction: ErgoTopBarAction? = null
+) {
+    CenterAlignedTopAppBar(
+        title = {
+            Text(
+                text = title,
+                fontWeight = FontWeight.Bold
+            )
+        },
+        navigationIcon = {
+            ErgoTopBarActionButton(rightAction)
+        },
+        actions = {
+            ErgoTopBarActionButton(leftAction)
+        }
+    )
+}
+
+@Composable
+fun ErgoTopBarActionButton(action: ErgoTopBarAction? = null) {
+    if (action != null) {
+        IconButton(
+            onClick = action.onClick
+        ) {
+            Icon(
+                imageVector = action.icon,
+                contentDescription = action.description
+            )
+        }
+    }
+}
