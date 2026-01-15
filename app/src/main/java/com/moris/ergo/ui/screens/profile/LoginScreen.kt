da8a4b642444d7cbb2a60e18d6cce8f850fdb4e6
moris071651 <moris071651@proton.me>
2026-01-15 09:06:31 +0200

    ADD: login screen

---
+++ app/src/main/java/com/moris/ergo/ui/screens/profile/LoginScreen.kt
@@ -0,0 +1,63 @@
+package com.moris.ergo.ui.screens.profile
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import com.moris.ergo.data.api.UserLoginRequestDTO
+
+@Composable
+fun LoginScreen(
+    viewModel: ProfileViewModel = hiltViewModel(),
+    onSuccess: () -> Unit
+) {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    Column(modifier = Modifier
+        .fillMaxSize()
+        .padding(16.dp)) {
+
+        OutlinedTextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text("Email") },
+            singleLine = true
+        )
+
+        Spacer(Modifier.height(8.dp))
+
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Password") },
+            singleLine = true,
+            visualTransformation = PasswordVisualTransformation()
+        )
+
+        Spacer(Modifier.height(16.dp))
+
+        Button(onClick = {
+            viewModel.login(
+                UserLoginRequestDTO(email, password),
+                onSuccess
+            )
+        }, modifier = Modifier.fillMaxWidth()) {
+            Text("Login")
+        }
+    }
+}
