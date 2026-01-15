aa327bebe0c05d58eb69efb36db71b00562807e9
moris071651 <moris071651@proton.me>
2026-01-15 09:03:01 +0200

    ADD: address dto

---
+++ app/src/main/java/com/moris/ergo/data/api/AddressDTO.kt
@@ -0,0 +1,24 @@
+package com.moris.ergo.data.api
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class AddressDTO(
+    val id: String,
+    val lon: Double,
+    val lat: Double,
+    val label: String,
+    val street: String? = null,
+    @SerialName("house_number") val houseNumber: String? = null,
+    val city: String? = null,
+    @SerialName("postal_code") val postalCode: String? = null,
+    val country: String? = null,
+)
+
+@Serializable
+data class CreateAddressRequestDTO(
+    val lon: Double,
+    val lat: Double,
+    val label: String
+)
---
+++ app/src/main/java/com/moris/ergo/data/scheme/Category.kt
@@ -0,0 +1,7 @@
+package com.moris.ergo.data.scheme
+
+data class Category(
+    val id: String,
+    val name: String,
+    val icon: String
+)
